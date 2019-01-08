344a672726212d1334d5ed1d75b7a842c82a4a35
wfping123 <wfping_123@163.com>
2019-01-08 17:16:26 +0800

    2019/01/08 增加状态集合2

--- src/main/java/com/thinkgem/jeesite/modules/infc/entity/DataStatusList.java
+++ src/main/java/com/thinkgem/jeesite/modules/infc/entity/DataStatusList.java
@@ -10,7 +10,7 @@ public class DataStatusList {
     private String success;        //是否成功
     private String statusMessage;  //状态信息
     //  private List<Object> data = Lists.newArrayList();	;            //数据集
-    private List<Map<String, Object>> mapList = Lists.newArrayList();     //数据
+    private List<Map<String, Object>> data = Lists.newArrayList();     //数据
 
     public DataStatusList(){
         this.success = "false";
@@ -32,11 +32,11 @@ public class DataStatusList {
         this.statusMessage = statusMessage;
     }
 
-    public List<Map<String, Object>> getMapList() {
-        return mapList;
+    public List<Map<String, Object>> getData() {
+        return data;
     }
 
-    public void setMapList(List<Map<String, Object>> mapList) {
-        this.mapList = mapList;
+    public void setData(List<Map<String, Object>> data) {
+        this.data = data;
     }
 }
